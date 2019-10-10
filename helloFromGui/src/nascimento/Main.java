f20b71047b936e1964a27b135b92fb4378d15eab
guibyiu <guimn@byui.edu>
2019-10-09 22:01:34 -0600

    First commit prove-04

---
+++ helloFromGui/src/nascimento/Main.java
@@ -0,0 +1,9 @@
+package nascimento;
+
+public class Main {
+
+    public static void main(String[] args){
+        String myName = "Gui";
+        System.out.println("Hello, my name is: " + myName);
+    }
+}
