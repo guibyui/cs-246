66993aa99a51e0f53057d57b697b476fd099d3e4
guibyiu <guimn@byui.edu>
2019-10-10 20:59:06 -0600

    Revert "I changed Gui's file"
    This reverts commit 2f47723b5c4d0b2795cbdd597dd0757055d11a20.

--- helloFromGui/src/nascimento/Main.java
+++ helloFromGui/src/nascimento/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args){
         String myName = "Gui";
         System.out.println("Hello, my name is: " + myName);
-        System.out.println("Gui is awesome!");
     }
 }
